c35ec0428035867e587f5caf463d798373c67598
Farhana Hossain <farhana9552@yahoo.com>
2018-05-21 04:54:05 +0000

    Task 2

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 /* JavaScript code here */
 //task1
 console.log("External JS")
+//task2
+
+function sumArray(arr){
+    var sum=0;
+    arr.forEach(function(value,index){
+        sum+=value;
+    })
+    return sum;
+}
+console.log(sumArray([1,2,3,4,5]));
